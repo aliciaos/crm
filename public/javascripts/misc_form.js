ff78017aa17207a9bc27f74c40b33c515beb2ba6
Santiago Pavon <santiago@dit.upm.es>
2017-03-02 17:09:08 +0100

    Añadido boton para poner la fecha de hoy en los campos de busquedas.

--- public/javascripts/misc_form.js
+++ public/javascripts/misc_form.js
@@ -1,3 +1,4 @@
+
 /**
  * Limpia los campos de un formulario.
  *
@@ -17,4 +18,24 @@ function cleanupForm(formId) {
         selects[i].value = "";
     }
 }
+
+/**
+ * Escribe la fecha de hoy en los campos de formulario pasados como argumento.
+ *
+ *
+ * @param date    String con la fecha a poner en los campos input dados en los siguientes argumentos.
+ * @param field1Id id del campo input donde queremos poner la fecha de hoy.
+ * @param field2Id id del campo input donde queremos poner la fecha de hoy.
+ * @param field3Id id del campo input donde queremos poner la fecha de hoy.
+ * @param field4Id etc.
+ */
+function setDate(date, field1Id, field2Id, field3Id, field4Id) {
+
+    for (var i = 1 ; i < arguments.length ; i++) {
+        var id = arguments[i];
+        var field = document.getElementById(id);
+        field.value = date;
+    }
+}
+
 
