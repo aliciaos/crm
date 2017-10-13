cf90c54024b7055d86bd2f328c17b8e0bf690d82
Santiago Pavon <santiago@dit.upm.es>
2017-10-13 12:17:30 +0200

    Borrar tabla Salesmen.

---
+++ migrations/20171013121200-DeleteSalesmemTable.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports = {
+    up: function (queryInterface, Sequelize) {
+        return queryInterface.dropTable('Salesmen');
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return queryInterface.createTable(
+            'Salesmen',
+            {   id: {
+                type: Sequelize.INTEGER,  allowNull: false,
+                primaryKey: true,         autoIncrement: true,
+                unique: true
+            },
+                name:  {
+                    type: Sequelize.STRING,
+                    unique: true,
+                    validate: { notEmpty: {msg: "Falta el nombre del vendedor."}}
+                },
+                createdAt: { type: Sequelize.DATE, allowNull: false },
+                updatedAt: { type: Sequelize.DATE, allowNull: false }
+            },
+            {    sync: {force: true}
+            }
+        );
+    }
+};
+
